8960b87137ee8672bf5b600c2698f86e9ad0f29d
Deisy Londoño <yoma8931@gmail.com>
2024-11-06 19:50:07 -0500

    se agrega el proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('titulo', 'mis productos')
+
+@section('contenido')
+<div
+class="hero min-h-screen"
+style="background-image: url(https://cdn.pixabay.com/photo/2024/05/31/05/24/trading-8799817_1280.png);">
+<div class="hero-overlay bg-opacity-10"></div>
+<div class="hero-content text-neutral-content text-center">
+  <div class="max-w-md">
+    <h1 class="mb-5 text-5xl font-bold">¡Bienvenido a DISTRIBUIDORA J&N, tu destino de compras en línea! </h1>
+    <p class="mb-5">
+      Descubre una experiencia de compra única con productos seleccionados para mejorar tu día a día. Desde tecnología de última generación hasta moda, hogar y bienestar, ¡tenemos todo lo que necesitas en un solo lugar! Con ofertas exclusivas, envíos rápidos y atención personalizada, comprar en J&N es fácil, seguro y conveniente.
+
+¿Qué estás esperando? ¡Explora nuestra tienda y encuentra eso que tanto deseas al mejor precio!.
+    </p>
+    <button class="btn btn-primary">Inicio</button>
+  </div>
+</div>
+</div>
+@endsection     
